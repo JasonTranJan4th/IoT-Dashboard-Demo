c7384b7abd6f8cb00c316e94ee41fb530cc3d1ba
Jason Tran <duycuong.drg@gmail.com>
2023-12-12 10:53:09 +0700

    config responsive

--- js/main.js
+++ js/main.js
@@ -20,9 +20,9 @@ function initBtnHeader(btnList) {
                 let data;
 
                 //const data1 = JSON.parse('{"ID":1,"Name":"Machine 1","Status":1,"Plan":2000,"Target":1000,"Actual":500,"NG":10,"OEE":{"Current":69.8,"IsInc":false,"PercentInc":-13.3,"Historical":[60.0,55.0,45.0,60.0,70.0,75.0,70.0,72.0,70.0,65.0]},"Availability":{"Current":85.0,"IsInc":true,"PercentInc":2.7,"Historical":[70.0,55.0,45.0,60.0,70.0,75.0,70.0,72.0,70.0,65.0]},"Performance":{"Current":86.4,"IsInc":false,"PercentInc":-15.4,"Historical":[60.0,65.0,45.0,69.0,70.0,75.0,76.0,72.0,88.0,65.0]},"Quality":{"Current":92.4,"IsInc":false,"PercentInc":-0.6,"Historical":[60.0,55.0,45.0,60.0,70.0,75.0,70.0,72.0,70.0,65.0]},"ProductionOrders":[{"No":"1","ID":"LSX-0001","ProductName":"Product 1","Target":105,"Actual":54,"NG":7,"Status":2},{"No":"2","ID":"LSX-0002","ProductName":"Product 2","Target":104,"Actual":54,"NG":6,"Status":2},{"No":"3","ID":"LSX-0003","ProductName":"Product 3","Target":104,"Actual":54,"NG":6,"Status":1},{"No":"4","ID":"LSX-0004","ProductName":"Product 4","Target":104,"Actual":54,"NG":6,"Status":0},{"No":"5","ID":"LSX-0005","ProductName":"Product 5","Target":107,"Actual":54,"NG":8,"Status":0}]}');
-                const data1 = JSON.parse('{"ID":1,"Name":"Machine 1","Status":1,"Plan":2000,"Target":1000,"Actual":500,"NG":10,"OEE":{"Current":69.8,"IsInc":false,"PercentInc":-13.3,"Historical":[60.0,55.0,45.0,60.0,70.0,75.0,70.0,72.0,70.0,65.0]},"Availability":{"Current":85.0,"IsInc":true,"PercentInc":2.7,"Historical":[70.0,55.0,45.0,60.0,70.0,75.0,70.0,72.0,70.0,65.0]},"Performance":{"Current":86.4,"IsInc":false,"PercentInc":-15.4,"Historical":[60.0,65.0,45.0,69.0,70.0,75.0,76.0,72.0,88.0,65.0]},"Quality":{"Current":92.4,"IsInc":false,"PercentInc":-0.6,"Historical":[60.0,55.0,45.0,60.0,70.0,75.0,70.0,72.0,70.0,65.0]},"ProductionOrders":[{"No":"1","ID":"LSX-0001","ProductName":"Product 1","Target":105,"Actual":54,"NG":7,"Status":2},{"No":"2","ID":"LSX-0002","ProductName":"Product 2","Target":104,"Actual":54,"NG":6,"Status":2},{"No":"3","ID":"LSX-0003","ProductName":"Product 3","Target":104,"Actual":54,"NG":6,"Status":1},{"No":"4","ID":"LSX-0004","ProductName":"Product 4","Target":104,"Actual":54,"NG":6,"Status":0}]}');
-                const data2 = JSON.parse('{"ID":1,"Name":"Machine 2","Status":1,"Plan":3000,"Target":2000,"Actual":600,"NG":11,"OEE":{"Current":70.8,"IsInc":false,"PercentInc":-13.3,"Historical":[70.0,55.0,90.0,60.0,70.0,75.0,70.0,72.0,70.0,65.0]},"Availability":{"Current":85.0,"IsInc":false,"PercentInc":2.7,"Historical":[80.0,55.0,45.0,60.0,70.0,75.0,70.0,72.0,70.0,65.0]},"Performance":{"Current":86.4,"IsInc":false,"PercentInc":-15.4,"Historical":[60.0,65.0,45.0,69.0,70.0,75.0,76.0,72.0,88.0,65.0]},"Quality":{"Current":92.4,"IsInc":false,"PercentInc":-0.6,"Historical":[60.0,55.0,45.0,60.0,70.0,75.0,70.0,72.0,70.0,65.0]},"ProductionOrders":[{"No":"1","ID":"LSX-0006","ProductName":"Product 6","Target":105,"Actual":54,"NG":7,"Status":2},{"No":"2","ID":"LSX-0007","ProductName":"Product 7","Target":104,"Actual":54,"NG":6,"Status":2},{"No":"3","ID":"LSX-0008","ProductName":"Product 8","Target":104,"Actual":54,"NG":6,"Status":1},{"No":"4","ID":"LSX-0009","ProductName":"Product 9","Target":104,"Actual":54,"NG":6,"Status":0},{"No":"5","ID":"LSX-00010","ProductName":"Product 10","Target":107,"Actual":54,"NG":8,"Status":0}]}');
-                const data3 = JSON.parse('{"ID":1,"Name":"Machine 3","Status":1,"Plan":4000,"Target":3000,"Actual":700,"NG":12,"OEE":{"Current":99.9,"IsInc":false,"PercentInc":-13.3,"Historical":[80.0,55.0,45.0,60.0,70.0,75.0,70.0,72.0,70.0,65.0]},"Availability":{"Current":85.0,"IsInc":true,"PercentInc":2.7,"Historical":[90.0,55.0,45.0,60.0,70.0,75.0,70.0,72.0,70.0,65.0]},"Performance":{"Current":86.4,"IsInc":false,"PercentInc":-15.4,"Historical":[60.0,65.0,45.0,69.0,70.0,75.0,76.0,72.0,88.0,65.0]},"Quality":{"Current":92.4,"IsInc":false,"PercentInc":-0.6,"Historical":[60.0,55.0,45.0,60.0,70.0,75.0,70.0,72.0,70.0,65.0]},"ProductionOrders":[{"No":"1","ID":"LSX-00011","ProductName":"Product 11","Target":105,"Actual":54,"NG":7,"Status":2},{"No":"2","ID":"LSX-00012","ProductName":"Product 12","Target":104,"Actual":54,"NG":6,"Status":2},{"No":"3","ID":"LSX-00013","ProductName":"Product 13","Target":104,"Actual":54,"NG":6,"Status":1},{"No":"4","ID":"LSX-00014","ProductName":"Product 14","Target":104,"Actual":54,"NG":6,"Status":0},{"No":"5","ID":"LSX-00015","ProductName":"Product 15","Target":107,"Actual":54,"NG":8,"Status":0}]}');
+                const data1 = JSON.parse('{"ID":1,"Name":"Machine 1","Status":1,"Plan":2000,"Target":1000,"Actual":500,"NG":10,"OEE":{"Current":69.8,"IsInc":false,"PercentInc":-13.3,"Historical":[60.0,55.0,45.0,60.0,70.0,75.0,70.0,72.0,70.0,65.0]},"Availability":{"Current":85.0,"IsInc":true,"PercentInc":2.7,"Historical":[70.0,55.0,45.0,60.0,70.0,75.0,70.0,72.0,70.0,65.0]},"Performance":{"Current":86.4,"IsInc":false,"PercentInc":-15.4,"Historical":[60.0,65.0,45.0,69.0,70.0,75.0,76.0,72.0,88.0,65.0]},"Quality":{"Current":92.4,"IsInc":false,"PercentInc":-0.6,"Historical":[60.0,55.0,45.0,60.0,70.0,75.0,70.0,72.0,70.0,65.0]},"ProductionOrders":[{"ID":"LSX-0001","ProductName":"Product 1","Target":105,"Actual":54,"NG":7,"Status":2},{"ID":"LSX-0002","ProductName":"Product 2","Target":104,"Actual":54,"NG":6,"Status":2},{"ID":"LSX-0003","ProductName":"Product 3","Target":104,"Actual":54,"NG":6,"Status":1},{"ID":"LSX-0004","ProductName":"Product 4","Target":104,"Actual":54,"NG":6,"Status":0}]}');
+                const data2 = JSON.parse('{"ID":1,"Name":"Machine 2","Status":1,"Plan":3000,"Target":2000,"Actual":600,"NG":11,"OEE":{"Current":70.8,"IsInc":false,"PercentInc":-13.3,"Historical":[70.0,55.0,90.0,60.0,70.0,75.0,70.0,72.0,70.0,65.0]},"Availability":{"Current":85.0,"IsInc":false,"PercentInc":2.7,"Historical":[80.0,55.0,45.0,60.0,70.0,75.0,70.0,72.0,70.0,65.0]},"Performance":{"Current":86.4,"IsInc":false,"PercentInc":-15.4,"Historical":[60.0,65.0,45.0,69.0,70.0,75.0,76.0,72.0,88.0,65.0]},"Quality":{"Current":92.4,"IsInc":false,"PercentInc":-0.6,"Historical":[60.0,55.0,45.0,60.0,70.0,75.0,70.0,72.0,70.0,65.0]},"ProductionOrders":[{"ID":"LSX-0006","ProductName":"Product 6","Target":105,"Actual":54,"NG":7,"Status":2},{"ID":"LSX-0007","ProductName":"Product 7","Target":104,"Actual":54,"NG":6,"Status":2},{"ID":"LSX-0008","ProductName":"Product 8","Target":104,"Actual":54,"NG":6,"Status":1},{"ID":"LSX-0009","ProductName":"Product 9","Target":104,"Actual":54,"NG":6,"Status":0},{"ID":"LSX-00010","ProductName":"Product 10","Target":107,"Actual":54,"NG":8,"Status":0}]}');
+                const data3 = JSON.parse('{"ID":1,"Name":"Machine 3","Status":1,"Plan":4000,"Target":3000,"Actual":700,"NG":12,"OEE":{"Current":99.9,"IsInc":false,"PercentInc":-13.3,"Historical":[80.0,55.0,45.0,60.0,70.0,75.0,70.0,72.0,70.0,65.0]},"Availability":{"Current":85.0,"IsInc":true,"PercentInc":2.7,"Historical":[90.0,55.0,45.0,60.0,70.0,75.0,70.0,72.0,70.0,65.0]},"Performance":{"Current":86.4,"IsInc":false,"PercentInc":-15.4,"Historical":[60.0,65.0,45.0,69.0,70.0,75.0,76.0,72.0,88.0,65.0]},"Quality":{"Current":92.4,"IsInc":false,"PercentInc":-0.6,"Historical":[60.0,55.0,45.0,60.0,70.0,75.0,70.0,72.0,70.0,65.0]},"ProductionOrders":[{"ID":"LSX-00011","ProductName":"Product 11","Target":105,"Actual":54,"NG":7,"Status":2},{"ID":"LSX-00012","ProductName":"Product 12","Target":104,"Actual":54,"NG":6,"Status":2},{"ID":"LSX-00013","ProductName":"Product 13","Target":104,"Actual":54,"NG":6,"Status":1},{"ID":"LSX-00014","ProductName":"Product 14","Target":104,"Actual":54,"NG":6,"Status":0},{"ID":"LSX-00015","ProductName":"Product 15","Target":107,"Actual":54,"NG":8,"Status":0}]}');
 
                 if (btnId === "1") {
                     data = data1;
@@ -71,7 +71,7 @@ function initBtnHeader(btnList) {
                  */
 
                 const oee = document.getElementById("oee");
-                oee.textContent = `${data.OEE.Current}%`;
+                oee.textContent = `${data.OEE.Current.toFixed(1)}%`;
 
                 const oeeTrend = document.getElementById("oee_trend");
                 oeeTrend.textContent = `Trend ${data.OEE.PercentInc}%`;
@@ -80,7 +80,7 @@ function initBtnHeader(btnList) {
                 oeeStatus.classList.add(data.OEE.IsInc === true ? "trend_increase" : "trend_decrease");
 
                 const perform = document.getElementById("perform");
-                perform.textContent = `${data.Performance.Current}%`;
+                perform.textContent = `${data.Performance.Current.toFixed(1)}%`;
 
                 const performTrend = document.getElementById("perform_trend");
                 performTrend.textContent = `Trend ${data.Performance.PercentInc}%`;
@@ -89,7 +89,8 @@ function initBtnHeader(btnList) {
                 performStatus.classList.add(data.Performance.IsInc === true ? "trend_increase" : "trend_decrease");
 
                 const avail = document.getElementById("avail");
-                avail.textContent = `${data.Availability.Current}%`;
+                avail.textContent = `${data.Availability.Current.toFixed(1)}%`;
+                //avail.textContent = data.Availability.Current.slice(-1) === "0" ? `${data.Availability.Current}.0%` : `${data.Availability.Current}%`;
 
                 const availTrend = document.getElementById("avail_trend");
                 availTrend.textContent = `Trend ${data.Availability.PercentInc}%`;
@@ -99,7 +100,7 @@ function initBtnHeader(btnList) {
                 availStatus.classList.add(data.Availability.IsInc === true ? "trend_increase" : "trend_decrease");
 
                 const quanlity = document.getElementById("quanlity");
-                quanlity.textContent = `${data.Quality.Current}%`;
+                quanlity.textContent = `${data.Quality.Current.toFixed(1)}%`;
 
                 const quanlityTrend = document.getElementById("quanlity_trend");
                 quanlityTrend.textContent = `Trend ${data.Quality.PercentInc}%`;
@@ -147,7 +148,10 @@ function initBtnHeader(btnList) {
                                 pEle.classList.remove("low", "normal", "high");
                                 const keyValue = pEle.dataset.id;
 
-                                if (keyValue === "Target" || keyValue === "Actual" || keyValue === "NG") {
+                                if (keyValue === "No") {
+                                    pEle.textContent = i + 1;
+                                }
+                                else if (keyValue === "Target" || keyValue === "Actual" || keyValue === "NG") {
                                     const value = Number.parseInt(data.ProductionOrders[i][keyValue]);
                                     if (value <= 50) {
                                         pEle.classList.add("low");
